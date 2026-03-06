1280ef8abd3b6042bb036394c47551b03ad7c7f4
Andrea Placuzzi <ANDREA.PLACUZZI@local.ittcesena.it>
2026-03-06 09:52:03 +0100

    creata funzione per generazione numeri

---
+++ Progetto Asincrono/Progetto Asincrono/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Progetto_Asincrono
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            generaNumeri();
+
+
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private async Task generaNumeri()
+        {
+             string[] alfa = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+            string lettere_alfabeto;
+            {
+                int Tmp = 0;
+                while (true)
+                {
+                    Tmp++;
+                    if (Tmp >= 25)
+                    {
+                        Tmp = 26;
+                    }
+                    await Task.Delay(100);
+                    lettere_alfabeto = alfa[Tmp];
+                    lblAlfabeto.Content = lettere_alfabeto;
+                }
+
+
+            }
+        }
+
+
+
+    }
+}
