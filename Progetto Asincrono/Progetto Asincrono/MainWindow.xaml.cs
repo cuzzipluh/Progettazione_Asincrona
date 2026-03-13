a8791449f64fd75d5d8692a0decb885270ca5b9e
Andrea Placuzzi <ANDREA.PLACUZZI@local.ittcesena.it>
2026-03-13 09:09:40 +0100

    versione migliorata

--- Progetto Asincrono/Progetto Asincrono/MainWindow.xaml.cs
+++ Progetto Asincrono/Progetto Asincrono/MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace Progetto_Asincrono
             generaNumeri();
         }
 
-        private async Task generaNumeri()
+        private async void generaNumeri()
         {
              string[] alfa = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
             {
